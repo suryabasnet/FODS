25d809f61ed1a62ba84e8be6641e86159aeb00b1
Dipanshu Agrawal <98246399+DipanshuAgrawal1@users.noreply.github.com>
2022-01-28 08:34:40 +0545

    Delete third.py

--- third.py
+++
@@ -1,8 +0,0 @@
-#to display first 5 rows from COVID-19 dataset.
-import pandas as pd
-covid_19_data= pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-17-2020.csv')
-print(covid_19_data)
-print("\nDataset information:")
-print(covid_19_data.info())
-print("\nMissing data information:")
-print(covid_19_data.isna().sum())
